e7f2cabcfedca20e2cae9a006001837eabb38f8b
Shelby Nelson <shelbeezy@email.arizona.edu>
2019-04-22 20:20:01 -0700

    The Final Program

---
+++ stdev_calc.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Author : shelbeezy
+Date   : 2019-04-22
+Purpose: Calculates the standard deviation of a sample set of numbers
+"""
+
+import argparse
+import sys
+import os
+from math import sqrt
+
+# --------------------------------------------------
+def get_args():
+    """get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Standard Deviation Calculator Help',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument(
+        'value', metavar='int', type=int, help='Enter number(s) with spaces between', nargs='+')
+
+    return parser.parse_args()
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    num_vals = args.value
+    length = len(num_vals)
+    sum_ = 0
+    sq_sum = 0
+
+    """Calculates the Mean"""       
+    for val in range(length):
+        sum_ += num_vals[val]
+    mean= sum_/length
+
+    """Calculates Variance and then Standard Deviation"""
+    for val in range(length):
+       sq_sum += pow((num_vals[val]-mean),2)
+    var = sq_sum/(length-1)
+    stdev = sqrt(var)
+    
+    print('The Mean is: {}\nThe Variance is: {}\nThe Standard Deviation is: {}'.format(mean, var, stdev))
+       
+       
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
